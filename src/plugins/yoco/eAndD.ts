38d8ff59587c1b7b5a6536bd89dbcb65890e14b6
Mitchell R <support@bettercorp.co.za>
2021-08-02 03:34:37 +0200

    Updating to BsB v5

--- src/plugins/yoco/eAndD.ts
+++ src/plugins/yoco/eAndD.ts
@@ -1,20 +1,19 @@
-import { PluginFeature } from '@bettercorp/service-base/lib/ILib';
+import { CPlugin } from '@bettercorp/service-base/lib/ILib';
 import * as crypto from 'crypto';
-import { YocoPluginConfig } from '../../lib';
 
 export class eAndD {
   private static readonly algorithm: string = 'aes-256-ctr';
-  static encrypt(features: PluginFeature, text: string) {
+  static encrypt(self: CPlugin, text: string) {
 
-    const cipher = crypto.createCipheriv(this.algorithm, features.getPluginConfig<YocoPluginConfig>().commsToken, Buffer.from(features.getPluginConfig<YocoPluginConfig>().commsTokenIV, 'hex'));
+    const cipher = crypto.createCipheriv(this.algorithm, self.getPluginConfig().commsToken, Buffer.from(self.getPluginConfig().commsTokenIV, 'hex'));
 
     const encrypted = Buffer.concat([cipher.update(text), cipher.final()]);
 
     return encrypted.toString('hex');
   }
-  static decrypt(features: PluginFeature, hash: string) {
+  static decrypt(self: CPlugin, hash: string) {
 
-    const decipher = crypto.createDecipheriv(this.algorithm, features.getPluginConfig<YocoPluginConfig>().commsToken, Buffer.from(features.getPluginConfig<YocoPluginConfig>().commsTokenIV, 'hex'));
+    const decipher = crypto.createDecipheriv(this.algorithm, self.getPluginConfig().commsToken, Buffer.from(self.getPluginConfig().commsTokenIV, 'hex'));
 
     const decrpyted = Buffer.concat([decipher.update(Buffer.from(hash, 'hex')), decipher.final()]);
 
--- src/plugins/yoco/plugin.ts
+++ src/plugins/yoco/plugin.ts
@@ -1,6 +1,6 @@
-import { IPlugin, PluginFeature } from '@bettercorp/service-base/lib/ILib';
+import { CPlugin, CPluginClient, IPlugin } from '@bettercorp/service-base/lib/ILib';
 import { Tools } from '@bettercorp/tools/lib/Tools';
-import { YocoDefaults, YocoPluginEvents, YocoSourcePluginEvents } from '../../lib';
+import { YocoDefaults, YocoPluginConfig, YocoPluginEvents, YocoSourcePluginEvents } from '../../lib';
 import { Request as ERequest, Response as EResponse } from 'express';
 import AXIOS from 'axios';
 import { AxiosResponse as AResponse } from 'axios';
@@ -9,56 +9,78 @@ import * as FS from 'fs';
 import * as PATH from 'path';
 import { eAndD } from './eAndD';
 import * as EXPRESS from 'express';
+import { express } from '@bettercorp/service-base-plugin-web-server/lib/plugins/express/express';
 
-export class Plugin implements IPlugin {
+export type PromiseResolve<TData = any, TReturn = void> = (data: TData) => TReturn;
+export class yoco extends CPluginClient<any> {
+  public readonly _pluginName: string = "payfast";
+  private _refPluginName: string;
+  constructor(self: IPlugin) {
+    super(self);
+    this._refPluginName = this.refPlugin.pluginName;
+  }
+
+  async ping(): Promise<boolean> {
+    return this.emitEventAndReturn(YocoPluginEvents.ping);
+  }
+
+  async startPaymentRequest(request: any): Promise<any> {
+    request.data.sourcePluginName = this._refPluginName;
+    return this.emitEventAndReturn(YocoPluginEvents.makePaymentRequest, request);
+  }
+
+  async onPaymentComplete(listener: (response: any) => void) {
+    this.refPlugin.onEvent(this._refPluginName, YocoSourcePluginEvents.paymentComplete, listener as any);
+  }
+}
+
+export class Plugin extends CPlugin<YocoPluginConfig> {
+    express!: express;
     private getYocoUrl(action: string): string {
         return `${ YocoDefaults.url }${ YocoDefaults.version }/${ action }/`;
     }
-    public init(features: PluginFeature): Promise<void> {
+    public init(): Promise<void> {
         const self = this;
         return new Promise((resolve) => {
-            features.initForPlugins('plugin-express', 'use', {
-                arg1: async (req: any, res: any, next: Function) => {
-                    features.log.debug(`REQ[${ req.method }] ${ req.path } (${ JSON.stringify(req.query) })`);
-                    res.setHeader('Access-Control-Allow-Headers', '*');
-                    res.setHeader('Access-Control-Allow-Origin', '*');
-                    res.setHeader('Access-Control-Allow-Methods', '*');
+            self.express = new express(self);
+            self.express.use(async (req: any, res: any, next: Function) => {
+                self.log.debug(`REQ[${ req.method }] ${ req.path } (${ JSON.stringify(req.query) })`);
+                res.setHeader('Access-Control-Allow-Headers', '*');
+                res.setHeader('Access-Control-Allow-Origin', '*');
+                res.setHeader('Access-Control-Allow-Methods', '*');
 
-                    if (req.method.toUpperCase() === 'OPTIONS')
-                        return res.sendStatus(200);
+                if (req.method.toUpperCase() === 'OPTIONS')
+                    return res.sendStatus(200);
 
-                    next();
-                }
+                next();
             });
-            features.log.info('USE JSON FOR EXPRESS');
-            features.initForPlugins('plugin-express', 'use', {
-                arg1: EXPRESS.json({ limit: '5mb' })
-            });
+            self.log.info('USE JSON FOR EXPRESS');
+            self.express.use(EXPRESS.json({ limit: '5mb' }));
 
-            features.onReturnableEvent(null, YocoPluginEvents.ping, (resolve, reject, data) => {
+            self.onReturnableEvent(null, YocoPluginEvents.ping, (resolve, reject, data) => {
                 if (Tools.isNullOrUndefined(data))
                     return reject('DATA UNDEFINED');
                 AXIOS.post(self.getYocoUrl('charges'), {}, {
                     headers: {
-                        'X-Auth-Secret-Key': features.getPluginConfig().sandboxConfig.secretKey
+                        'X-Auth-Secret-Key': self.getPluginConfig().sandboxConfig.secretKey
                     }
                 }).then(x => {
                     resolve(x.status === 400 && x.data.errorType === 'invalid_request_error');
                 }).catch(x => {
-                    features.log.error(x);
+                    self.log.error(x);
                     resolve(false);
                 });
             });
-            features.onReturnableEvent(null, YocoPluginEvents.makePaymentRequest, (resolve, reject, data) => {
+            self.onReturnableEvent(null, YocoPluginEvents.makePaymentRequest, (resolve, reject, data) => {
                 if (Tools.isNullOrUndefined(data))
                     return reject('DATA UNDEFINED');
-                let merchantConfig = features.getPluginConfig().sandboxConfig;
+                let merchantConfig = self.getPluginConfig().sandboxConfig;
                 if (data.client.live === true) {
                     merchantConfig.publicKey = data.client.publicKey;
                     merchantConfig.secretKey = data.client.secretKey;
                 }
                 try {
-                    let amountInCents = Number.parseInt((data.data.amount * 100).toFixed(0))
+                    let amountInCents = Number.parseInt((data.data.amount * 100).toFixed(0));
                     let workingObj = {
                         time: new Date().getTime(),
                         timeExpiry: new Date().getTime() + (1000 * 60 * 60),
@@ -84,7 +106,7 @@ export class Plugin implements IPlugin {
                         authTagLength: 16
                     });
                     let requestKey = Buffer.concat([requestCipher.update(JSON.stringify(workingObj)), requestCipher.final()]).toString('base64');*/
-                    let requestKey = eAndD.encrypt(features, JSON.stringify(workingObj));
+                    let requestKey = eAndD.encrypt(self, JSON.stringify(workingObj));
                     /*let key: any = {
                       char1: Math.floor((Math.random() * 5) + 1),
                       char2: Math.floor((Math.random() * 5) + 1) + 5,
@@ -96,7 +118,7 @@ export class Plugin implements IPlugin {
                     requestKey[key.char1] = `${Math.floor((Math.random() * 9) + 1)}`[0];
                     requestKey[key.char2] = `${Math.floor((Math.random() * 9) + 1)}`[0];*/
                     return resolve({
-                        url: `${ features.getPluginConfig().myHost }/Yoco/${ encodeURIComponent(requestKey) }`,
+                        url: `${ self.getPluginConfig().myHost }/Yoco/${ encodeURIComponent(requestKey) }`,
                         request: {
                             time: new Date().getTime(),
                             timeExpiry: 0,
@@ -121,7 +143,7 @@ export class Plugin implements IPlugin {
                     });
                 }
                 catch (erc) {
-                    features.log.error(erc);
+                    self.log.error(erc);
                     return reject(erc);
                 }
             });
@@ -129,115 +151,109 @@ export class Plugin implements IPlugin {
         });
     }
     public readonly loadedIndex: number = 999995;
-    public loaded(features: PluginFeature): Promise<void> {
+    public loaded(): Promise<void> {
         const self = this;
         return new Promise((resolve) => {
-            features.log.debug(`loaded`);
-            features.initForPlugins('plugin-express', 'get', {
-                arg1: '/Yoco/:token',
-                arg2: (req: ERequest, res: EResponse) => {
-                    try {
-                        /*const cipherText = Buffer.from(decodeURIComponent(req.params.token), "base64");
-                        const cipher = crypto.createDecipheriv("aes-256-ccm", Buffer.from(features.getPluginConfig().commsToken, 'hex'), crypto.pseudoRandomBytes(6).toString('hex'), {
-                            authTagLength: 16
-                        });
-                        let decrypted = Buffer.concat([cipher.update(cipherText), cipher.final()]).toString('utf8');*/
-                        let decrypted = eAndD.decrypt(features, decodeURIComponent(req.params.token));
-                        let data = JSON.parse(decrypted);
-                        let now = new Date().getTime();
-                        if (now >= data.timeExpiry)
-                            throw 'Time expired!';
-                        let content = FS.readFileSync(PATH.join(features.cwd, './src/plugins/yoco/content/index.html')).toString();
-                        let variablesToClient = {
-                            url: features.getPluginConfig().myHost + '/Yoco/' + req.params.token,
-                            amountFormatted: data.amountFormatted,
-                            amountCents: data.amountInCents,
-                            currency: data.currency,
-                            publicKey: data.publicKey,
-                            description: data.paymentReference,
-                            metadata: {
-                                internalReference: data.paymentInternalReference
-                            },
-                            customer: {
-                                email: data.email,
-                                phone: data.cell,
+            self.log.debug(`loaded`);
+            self.express.get('/Yoco/:token', (req: ERequest, res: EResponse) => {
+                try {
+                    /*const cipherText = Buffer.from(decodeURIComponent(req.params.token), "base64");
+                    const cipher = crypto.createDecipheriv("aes-256-ccm", Buffer.from(features.getPluginConfig().commsToken, 'hex'), crypto.pseudoRandomBytes(6).toString('hex'), {
+                        authTagLength: 16
+                    });
+                    let decrypted = Buffer.concat([cipher.update(cipherText), cipher.final()]).toString('utf8');*/
+                    let decrypted = eAndD.decrypt(self, decodeURIComponent(req.params.token));
+                    let data = JSON.parse(decrypted);
+                    let now = new Date().getTime();
+                    if (now >= data.timeExpiry)
+                        throw 'Time expired!';
+                    let content = FS.readFileSync(PATH.join(self.cwd, './src/plugins/yoco/content/index.html')).toString();
+                    let variablesToClient = {
+                        url: self.getPluginConfig().myHost + '/Yoco/' + req.params.token,
+                        amountFormatted: data.amountFormatted,
+                        amountCents: data.amountInCents,
+                        currency: data.currency,
+                        publicKey: data.publicKey,
+                        description: data.paymentReference,
+                        metadata: {
+                            internalReference: data.paymentInternalReference
+                        },
+                        customer: {
+                            email: data.email,
+                            phone: data.cell,
+                            firstName: data.firstName,
+                            lastName: data.lastName
+                        }
+                    };
+                    content = content.replace('{{VARIABLES}}', JSON.stringify(variablesToClient));
+                    res.setHeader('content-type', 'text/html');
+                    res.send(content);
+                }
+                catch (xcc) {
+                    self.log.error(xcc);
+                    res.status(400).send('An unknown error occurred');
+                }
+            });
+            self.log.debug(`loaded`);
+            self.express.post('/Yoco/:token', (req: ERequest, res: EResponse) => {
+                try {
+                    /*const cipherText = Buffer.from(decodeURIComponent(req.params.token), "base64");
+                    const cipher = crypto.createDecipheriv("aes-256-ccm", Buffer.from(features.getPluginConfig().commsToken, 'hex'), crypto.pseudoRandomBytes(6).toString('hex'), {
+                        authTagLength: 16
+                    });
+                    let decrypted = Buffer.concat([cipher.update(cipherText), cipher.final()]).toString('utf8');*/
+                    let decrypted = eAndD.decrypt(self, decodeURIComponent(req.params.token));
+                    let data = JSON.parse(decrypted);
+                    let reqData = req.body;
+                    console.log(reqData);
+                    let now = new Date().getTime();
+                    if (now >= data.timeExpiry)
+                        throw 'Time expired!';
+                    self.log.debug(data);
+                    AXIOS.post(self.getYocoUrl('charges'), {
+                        token: reqData.id,
+                        amountInCents: data.amountInCents,
+                        currency: data.currency
+                    }, {
+                        headers: {
+                            'X-Auth-Secret-Key': data.secretKey
+                        }
+                    }).then((x: AResponse): any | void => {
+                        if (x.status === 201) {
+                            self.emitEvent(data.notifyService, YocoSourcePluginEvents.paymentComplete, {
+                                publicKey: data.publicKey,
+                                amount: data.amountInCents,
+                                paymentReference: data.paymentReference,
+                                paymentInternalReference: data.paymentInternalReference,
+                                currency: data.currency,
                                 firstName: data.firstName,
-                                lastName: data.lastName
-                            }
-                        };
-                        content = content.replace('{{VARIABLES}}', JSON.stringify(variablesToClient));
-                        res.setHeader('content-type', 'text/html');
-                        res.send(content);
-                    }
-                    catch (xcc) {
-                        features.log.error(xcc);
-                        res.status(400).send('An unknown error occurred');
-                    }
+                                lastName: data.lastName,
+                                email: data.email,
+                                cell: data.cell,
+                                payment: {
+                                    req: reqData,
+                                    res: x.data
+                                }
+                            });
+                            return res.status(201).send({
+                                status: x.data.status === 'successful',
+                                reference: data.paymentReference,
+                                internalReference: data.paymentInternalReference
+                            });
+                        }
+                        self.log.error(x.data);
+                        res.status(500).send({ status: false, message: x.data.displayMessage });
+                    }).catch(x => {
+                        self.log.error(x.response.data);
+                        res.status(500).send({ status: false, message: x.response.data.displayMessage });
+                    });
+                }
+                catch (xcc) {
+                    self.log.error(xcc);
+                    res.status(400).send({ status: false, message: 'An unknown error occurred' });
                 }
             });
-            features.log.debug(`loaded`);
-            features.initForPlugins('plugin-express', 'post', {
-                arg1: '/Yoco/:token',
-                arg2: (req: ERequest, res: EResponse) => {
-                    try {
-                        /*const cipherText = Buffer.from(decodeURIComponent(req.params.token), "base64");
-                        const cipher = crypto.createDecipheriv("aes-256-ccm", Buffer.from(features.getPluginConfig().commsToken, 'hex'), crypto.pseudoRandomBytes(6).toString('hex'), {
-                            authTagLength: 16
-                        });
-                        let decrypted = Buffer.concat([cipher.update(cipherText), cipher.final()]).toString('utf8');*/
-                        let decrypted = eAndD.decrypt(features, decodeURIComponent(req.params.token));
-                        let data = JSON.parse(decrypted);
-                        let reqData = req.body;
-                        console.log(reqData);
-                        let now = new Date().getTime();
-                        if (now >= data.timeExpiry)
-                            throw 'Time expired!';
-                        features.log.debug(data);
-                        AXIOS.post(self.getYocoUrl('charges'), {
-                            token: reqData.id,
-                            amountInCents: data.amountInCents,
-                            currency: data.currency
-                        }, {
-                            headers: {
-                                'X-Auth-Secret-Key': data.secretKey
-                            }
-                        }).then((x: AResponse): any | void => {
-                            if (x.status === 201) {
-                                features.emitEvent(data.notifyService, YocoSourcePluginEvents.paymentComplete, {
-                                    publicKey: data.publicKey,
-                                    amount: data.amountInCents,
-                                    paymentReference: data.paymentReference,
-                                    paymentInternalReference: data.paymentInternalReference,
-                                    currency: data.currency,
-                                    firstName: data.firstName,
-                                    lastName: data.lastName,
-                                    email: data.email,
-                                    cell: data.cell,
-                                    payment: {
-                                        req: reqData,
-                                        res: x.data
-                                    }
-                                });
-                                return res.status(201).send({
-                                    status: x.data.status === 'successful',
-                                    reference: data.paymentReference,
-                                    internalReference: data.paymentInternalReference
-                                });
-                            }
-                            features.log.error(x.data);
-                            res.status(500).send({ status: false, message: x.data.displayMessage });
-                        }).catch(x => {
-                            features.log.error(x.response.data);
-                            res.status(500).send({ status: false, message: x.response.data.displayMessage });
-                        });
-                    }
-                    catch (xcc) {
-                        features.log.error(xcc);
-                        res.status(400).send({ status: false, message: 'An unknown error occurred' });
-                    }
-                }
-            });
-            features.log.debug('YOCO READY');
+            self.log.debug('YOCO READY');
             resolve();
         });
     };
